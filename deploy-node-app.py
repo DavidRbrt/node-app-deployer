d55ccbd7e4539176d51da655413f393cbe642f23
David <robert.david.mickael@gmail.com>
2020-04-29 22:05:17 +0200

    todo

--- deploy-node-app.py
+++ deploy-node-app.py
@@ -304,6 +304,7 @@ if __name__ == "__main__":
         repo.git.clean('-xdf')
         # pull in the changes from from the remote
         repo.remotes.origin.pull('--force')
+        # TODO: git submodule update --recursive --remote
         ##########################################
 
     if do_install:
